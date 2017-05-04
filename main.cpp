47e0493716e4aeaa60043bcacfaeaa259b839066
jinixin <jixinmanage@qq.com>
2017-05-04 14:35:59 +0800

    增加程序初始化，优化高频单词的统计
    Signed-off-by: jinixin <jixinmanage@qq.com>

--- main.cpp
+++ main.cpp
@@ -210,13 +210,20 @@ void chajieguo(int fl)
         printf("\n  单词                 总共出现次数    千分比（占所有文章）\n");
         for(i=0;i<k;i++)
         {
-            if(zu[i].bj*2>wzzs)
+            if(wzzs>3)
             {
-                strcpy(q,"超高频");
-            }
-            else if(zu[i].bj*3>wzzs)
-            {
-                strcpy(q,"高频");
+                if(zu[i].bj*2>wzzs)
+                {
+                    strcpy(q,"超高频");
+                }
+                else if(zu[i].bj*3>wzzs)
+                {
+                    strcpy(q,"高频");
+                }
+                else
+                {
+                    strcpy(q,"");
+                }
             }
             else
             {
@@ -284,13 +291,20 @@ void cundang(int fl)
         {
             if(!shanchufeng(zu[i].p))
             {
-                if(zu[i].bj*2>wzzs)
+                if(wzzs>3)
                 {
-                    strcpy(q,"超高频");
-                }
-                else if(zu[i].bj*3>wzzs)
-                {
-                    strcpy(q,"高频");
+                    if(zu[i].bj*2>wzzs)
+                    {
+                        strcpy(q,"超高频");
+                    }
+                    else if(zu[i].bj*3>wzzs)
+                    {
+                        strcpy(q,"高频");
+                    }
+                    else
+                    {
+                        strcpy(q,"");
+                    }
                 }
                 else
                 {
@@ -510,13 +524,20 @@ void sousuo(int fl)
             {
                 printf("\n  ******已找到您要查找的%s单词******\n",dc);
                 printf("\n  单词                 总共出现次数    千分比（占所有文章）\n");
-                if(zu[i].bj*2>wzzs)
+                if(wzzs>3)
                 {
-                    strcpy(q,"超高频");
-                }
-                else if(zu[i].bj*3>wzzs)
-                {
-                    strcpy(q,"高频");
+                    if(zu[i].bj*2>wzzs)
+                    {
+                        strcpy(q,"超高频");
+                    }
+                    else if(zu[i].bj*3>wzzs)
+                    {
+                        strcpy(q,"高频");
+                    }
+                    else
+                    {
+                        strcpy(q,"");
+                    }
                 }
                 else
                 {
@@ -532,10 +553,78 @@ void sousuo(int fl)
         }
     }
 }
+void chushihua()
+{
+    FILE *fp;
+    if((fp=fopen("amain(system).in","w"))==NULL)
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    fprintf(fp,"0");
+    if(fclose(fp))
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if((fp=fopen("bzong(system).out","w"))==NULL)
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if(fclose(fp))
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if((fp=fopen("calrea(system).in","w"))==NULL)
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    fprintf(fp,"a 0");
+    if(fclose(fp))
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if((fp=fopen("所有文章单词数.txt","w"))==NULL)
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if(fclose(fp))
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if((fp=fopen("新文章.txt","w"))==NULL)
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if(fclose(fp))
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if((fp=fopen("新文章单词数.txt","w"))==NULL)
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    if(fclose(fp))
+    {
+        printf("\n对不起，程序发生崩溃，现已为您安全结束程序\n\n请您按任意键退出本对话框\n");
+        exit(0);
+    }
+    printf("\n******初始化程序（删除所有数据）成功******\n");
+    printf("\n******在此自动为您返回上级******\n");
+}
 int main()
 {
     int xz1,xz2;
-    printf("**********************************************\n\n\n\n\n\n\n          欢迎使用词数统计程序  V2.1\n\n\n\n\n\n\n**********************************************\n\n");
+    printf("**********************************************\n\n\n\n\n\n\n          欢迎使用词数统计程序  V2.2\n\n\n\n\n\n\n**********************************************\n\n");
     shanchuzong();
     while(1)
     {
@@ -610,7 +699,7 @@ int main()
             duruzongshu();
             while(1)
             {
-                printf("\n2.查看结果\n3.排序单词\n4.搜索单词\n0.返回上级\n\n请选择：");
+                printf("\n2.查看结果\n3.排序单词\n4.搜索单词\n5.初始化程序（删除所有数据-慎选）\n0.返回上级\n\n请选择：");
                 scanf("%d",&xz2);
                 if(xz2!=0&&k==0)
                 {
@@ -629,6 +718,10 @@ int main()
                 else if(xz2==4)
                 {
                     sousuo(2);
+                }
+                else if(xz2==5)
+                {
+                    chushihua();
                 }
                 else if(xz2==0)
                 {
@@ -696,21 +789,21 @@ int main()
             month[0]=time[0];month[1]=time[1];month[2]='\0';
             year[0]='2';year[1]='0';year[2]=time[6];year[3]=time[7];year[4]='\0';
             _strtime(time);
-            printf("\n********************************************************\n");
+            printf("\n******************************************************\n");
             printf("程序名称    ：单词个数统计程序\n");
-            printf("程序版本    ：V2.1\n");
+            printf("程序版本    ：V2.2\n");
             printf("内核版本    ：V1.4（基于优化V1.0）\n");
-            printf("迭代次数    ：12\n");
+            printf("迭代次数    ：13\n");
             printf("开发语言    ：C/C++\n");
             printf("开发所属    ：AEM 工程\n");
             printf("开发者      ：Bei feng\n");
             printf("系统当前时间：%s\n",time);
             printf("系统当前日期：%s/%s/%s\n",year,month,day);
             printf("初次开发日期：2014/07/30\n");
-            printf("最后更新日期：2014/10/03\n");
+            printf("最后更新日期：2014/10/04\n");
             printf("**********************************\n");
             printf("\n如有任何改进意见，请联系我们：jinixinwork@sina.com\n");
-            printf("\n********************************************************\n");
+            printf("\n******************************************************\n");
         }
         else if(xz1==5)
         {
